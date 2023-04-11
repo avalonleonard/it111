cd86bb3cca55a208ee05822057458e5b36f79211
avalonleonard <129801280+avalonleonard@users.noreply.github.com>
2023-04-10 19:42:38 -0700

    Add files via upload

---
+++ week2/src/Hello.java
@@ -0,0 +1,8 @@
+// the infamous hello world example
+public class Hello {
+
+    public static void main (String[] args) {
+
+        System.out.println("The famous Hello World Example");
+    } // end main
+} // end
